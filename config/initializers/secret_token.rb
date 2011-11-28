5022a34188a8c94722c1f9525e97879f0a7d378b
Anup Mahabudhe <anupwin@gmail.com>
2011-11-28 16:01:49 +0530

    Registration process for User

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Floorsys::Application.config.secret_token = '8ca6db8a6ed69a655fd24392ac5a2c4644dcdb8e2342dff366e26e00b41066ecbe11399e102a5ff121a4bf9ecb68d4bf0d1930724ba796dd0387d649dbc7a2fe'
